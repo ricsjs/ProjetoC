5ac9a8de39d09982ad6cfa437487afe8a1c253f0
Ricardo <ricfilho0007@gmail.com>
2022-08-27 09:05:48 -0300

    upload do arquivo principal

---
+++ principal.c
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///             Developed by @ricsjs & @janderson1111 -- since Ago, 2022    ///
+///////////////////////////////////////////////////////////////////////////////
+///                                Semana 1                                 ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void tela_login(void);
+void tela_principal(void);
+void tela_sobre(void);
+
+int main (void){
+    tela_login();
+    tela_principal();
+    tela_sobre();
+    return  0 ;
+}
+
+void tela_login(void){
+
+}
+
+void tela_principal(void){
+
+}
+
+void tela_sobre(void){
+    
+}
+
