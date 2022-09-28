2c3d45aa2f0a92aded018750776978bdba56687a
janderson natan <jandersonnatan.92@gmail.com>
2022-09-27 21:55:40 -0300

    padronização das telas

--- principal.c
+++ principal.c
@@ -27,7 +27,7 @@ void remover_evento(void);
 int main(void){
     setlocale(LC_ALL,"portuguese");
     login();
-    printf("programa finalizado");
+    printf("Programa finalizado");
 }
 
 
@@ -35,11 +35,11 @@ void login(void){
 
     char user, pass;
 
-    printf("==================================\n");
-    printf("|                                |\n");
-    printf("| OLÁ, SEJA BEM-VINDO AO SIG-LAW |\n");
-    printf("|                                |\n");
-    printf("==================================\n");
+    printf("////////////////////////////////////////\n");
+    printf("///                                  ///\n");
+    printf("///  OLÁ, SEJA BEM-VINDO AO SIG-LAW  ///\n");
+    printf("///                                  ///\n");
+    printf("////////////////////////////////////////\n");
 
     printf("Digite seu nome de usuário: \n");
     scanf("%s", &user);
@@ -67,28 +67,29 @@ void login(void){
  void principal(void){
     int opc;
     while (opc !=0){
-
-        printf("=============================================================\n");
-        printf("|                         Semana 5                          |\n");
-        printf("=============================================================\n");
-        printf("|                                                           |\n");
-        printf("|    Universidade Federal do Rio Grande do Norte            |\n");
-        printf("|    Centro de Ensino Superior do Seridó                    |\n");
-        printf("|    Departamento de Computação e Tecnologia                |\n");
-        printf("|    Disciplina DCT1106 -- Programação                      |\n");
-        printf("|    Projeto                                                |\n");
-        printf("|    Sistema de agendamento para escritório de advocacia    |\n");
-        printf("|                                                           |\n");
-        printf("=============================================================\n");
-        printf("=============================================================\n");
-        printf("|                            MENU                           |\n");
-        printf("=============================================================\n");
-        printf("| 1 - ÁREA DO ADVOGADO;                                     |\n");
-        printf("| 2 - ÁREA DO CLIENTE;                                      |\n");
-        printf("| 3 - AGENDA GERAL;                                         |\n");
-        printf("| 4 - SOBRE O PROGRAMA;                                     |\n");
-        printf("| 0 - SAIR;                                                 |\n");
-        printf("=============================================================\n");
+        system("clear||cls");
+        printf("/////////////////////////////////////////////////////////////////\n");
+        printf("///             ========== Semana 5 ==========                ///\n");
+        printf("/////////////////////////////////////////////////////////////////\n");
+        printf("///                                                           ///\n");
+        printf("///  Universidade Federal do Rio Grande do Norte              ///\n");
+        printf("///  Centro de Ensino Superior do Seridó                      ///\n");
+        printf("///  Departamento de Computação e Tecnologia                  ///\n");
+        printf("///  Disciplina DCT1106 -- Programação                        ///\n");
+        printf("///  Projeto                                                  ///\n");
+        printf("///  Sistema de agendamento para escritório de advocacia      ///\n");
+        printf("///                                                           ///\n");
+        printf("/////////////////////////////////////////////////////////////////\n");
+        printf("///               ========== MENU ==========                  ///\n");
+        printf("/////////////////////////////////////////////////////////////////\n");
+        printf("///                                                           ///\n");
+        printf("///  1 - ÁREA DO ADVOGADO;                                    ///\n");
+        printf("///  2 - ÁREA DO CLIENTE;                                     ///\n");
+        printf("///  3 - AGENDA GERAL;                                        ///\n");
+        printf("///  4 - SOBRE O PROGRAMA;                                    ///\n");
+        printf("///  0 - SAIR;                                                ///\n");
+        printf("///                                                           ///\n");
+        printf("/////////////////////////////////////////////////////////////////\n");
 
         printf("Digite o número de uma opção: ");
         scanf("%i", &opc);
@@ -121,17 +122,18 @@ void area_advogado(void){
     int opc;
     while (opc != 0){
     system("clear||cls");
-    printf("==========================================\n");
-    printf("|             ÁREA DO ADVOGADO           |\n");
-    printf("==========================================\n");
-    printf("|                                        |\n");
-    printf("| 1 - PESQUISAR ADVOGADO                 |\n");
-    printf("| 2 - CADASTRAR ADVOGADO                 |\n");
-    printf("| 3 - EDITAR ADVOGADO                    |\n");
-    printf("| 4 - AGENDA DO ADVOGADO                 |\n");
-    printf("| 5 - REMOVER ADVOGADO                   |\n");
-    printf("| 0 - VOLTAR                             |\n");
-    printf("==========================================\n");
+    printf("/////////////////////////////////////////////\n");
+    printf("///      ===== ÁREA DO ADVOGADO =====     ///\n");
+    printf("/////////////////////////////////////////////\n");
+    printf("///                                       ///\n");
+    printf("///  1 - PESQUISAR ADVOGADO               ///\n");
+    printf("///  2 - CADASTRAR ADVOGADO               ///\n");
+    printf("///  3 - EDITAR ADVOGADO                  ///\n");
+    printf("///  4 - AGENDA DO ADVOGADO               ///\n");
+    printf("///  5 - REMOVER ADVOGADO                 ///\n");
+    printf("///  0 - VOLTAR                           ///\n");
+    printf("///                                       ///\n");
+    printf("/////////////////////////////////////////////\n");
 
     printf("Digite o número de uma opção: ");
     scanf("%i", &opc);
@@ -311,17 +313,18 @@ void area_cliente(void){
     int opc;
     while (opc !=0 ){
     system("clear||cls");
-    printf("==========================================\n");
-    printf("|             ÁREA DO CLIENTE            |\n");
-    printf("==========================================\n");
-    printf("|                                        |\n");
-    printf("| 1 - PESQUISAR CLIENTE                  |\n");
-    printf("| 2 - CADASTRAR CLIENTE                  |\n");
-    printf("| 3 - EDITAR CLIENTE                     |\n");
-    printf("| 4 - AGENDA DO CLIENTE                  |\n");
-    printf("| 5 - REMOVER CLIENTE                    |\n");
-    printf("| 0 - VOLTAR                             |\n");
-    printf("==========================================\n");
+    printf("/////////////////////////////////////////////\n");
+    printf("///      ===== ÁREA DO CLIENTE =====      ///\n");
+    printf("/////////////////////////////////////////////\n");
+    printf("///                                       ///\n");
+    printf("///  1 - PESQUISAR CLIENTE                ///\n");
+    printf("///  2 - CADASTRAR CLIENTE                ///\n");
+    printf("///  3 - EDITAR CLIENTE                   ///\n");
+    printf("///  4 - AGENDA DO CLIENTE                ///\n");
+    printf("///  5 - REMOVER CLIENTE                  ///\n");
+    printf("///  0 - VOLTAR                           ///\n");
+    printf("///                                       ///\n");
+    printf("/////////////////////////////////////////////\n");
 
     printf("Digite o número de uma opção: ");
     scanf("%i", &opc);
@@ -520,16 +523,16 @@ void agenda_geral(void){
     int opc;
     while (opc != 0){
     system("clear||cls");
-    printf("==========================================\n");
-    printf("|             AGENDA GERAL               |\n");
-    printf("==========================================\n");
-    printf("|                                        |\n");
-    printf("| 1 - LISTAR EVENTOS                     |\n");
-    printf("| 2 - CADASTRAR NOVO EVENTO              |\n");
-    printf("| 3 - EDITAR EVENTO                      |\n");
-    printf("| 4 - REMOVER EVENTO                     |\n");
-    printf("| 0 - VOLTAR                             |\n");
-    printf("==========================================\n");
+    printf("/////////////////////////////////////////////\n");
+    printf("///       ===== AGENDA GERAL =====        ///\n");
+    printf("/////////////////////////////////////////////\n");
+    printf("///                                       ///\n");
+    printf("///  1 - LISTAR EVENTOS                   ///\n");
+    printf("///  2 - CADASTRAR NOVO EVENTO            ///\n");
+    printf("///  3 - EDITAR EVENTO                    ///\n");
+    printf("///  4 - REMOVER EVENTO                   ///\n");
+    printf("///  0 - VOLTAR                           ///\n");
+    printf("/////////////////////////////////////////////\n");
 
     printf("Digite o número de uma opção: ");
     scanf("%i", &opc);
@@ -685,48 +688,42 @@ void remover_evento(void){
 
 void sobre(void){
     system("clear||cls");
-    printf("==========================================\n");
-    printf("|            Sobre o Projeto             |\n");
-    printf("==========================================\n");
-    printf("|     Equipe que realizou o projeto      |\n");
-    printf("==========================================\n");
-    printf("|                                        |\n");
-    printf("|         Alunos: Janderson Natan        |\n");
-    printf("|         & Ricardo Alencar              |\n");
-    printf("|                                        |\n");
-    printf("|         Alunos do curso de BSI         |\n");
-    printf("|  Bacharelado de Sistemas de Informação |\n");
-    printf("|                                        |\n");
-    printf("|    Instagram: @janderson_natan         |\n");
-    printf("|    e @eualencaar                       |\n");
-    printf("|                                        |\n");
-    printf("|          Github: @janderson1111        |\n");
-    printf("|          e @ricsjs                     |\n");
-    printf("|                                        |\n");
-    printf("==========================================\n");
-    printf("|                                        |\n");
-    printf("|         Finalidade do Projeto          |\n");
-    printf("|                                        |\n");
-    printf("==========================================\n");
-    printf("|                                        |\n");
-    printf("| O projeto foi desgninado pelo professor|\n");
-    printf("| Flavius Gorgônio que leciona a dicipli-|\n");
-    printf("| na de Programação - DCT1106 na UFRN.   |\n");
-    printf("| O tema do nosso projeto é criar um sis-|\n");
-    printf("| tema de agendamento para escritório de |\n");
-    printf("| advocacia. Esse sistema deve ser criado|\n");
-    printf("| na linguagem C, ele servirá para faci- |\n");
-    printf("| litar o agendamento de reuniões e even-|\n");
-    printf("| tos do setor jurídico, para que não    |\n");
-    printf("| hajam imprevistos com horários.        |\n");
-    printf("|                                        |\n");
-    printf("| O sistema deve contar com:             |\n");
-    printf("| ÁREA DO ADVOGADO;                      |\n");
-    printf("| ÁREA DO CLIENTE;                       |\n");
-    printf("| AGENDA GERAL;                          |\n");
-    printf("| SOBRE O PROGRAMA;                      |\n");
-    printf("|                                        |\n");
-    printf("==========================================\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                            Sobre o Projeto                           ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                     Equipe que realizou o projeto                    ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                      ///\n");
+    printf("///              Alunos: Janderson Natan & Ricardo Alencar               ///\n");
+    printf("///                                                                      ///\n");
+    printf("///    Alunos do curso de BSI (Bacharelado de Sistemas de Informação)    ///\n");
+    printf("///                                                                      ///\n");
+    printf("///               Instagram: @janderson_natan e @eualencaar              ///\n");
+    printf("///                                                                      ///\n");
+    printf("///                  Github: @janderson1111 e @ricsjs                    ///\n");
+    printf("///                                                                      ///\n");
+    printf("///                                                                      ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                      ///\n");
+    printf("///             ========== Finalidade do Projeto ==========              ///\n");
+    printf("///                                                                      ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                      ///\n");
+    printf("///    O projeto foi desgninado pelo professor Flavius Gorgônio          ///\n");
+    printf("///    que leciona a diciplina de Programação - DCT1106 na UFRN.         ///\n");
+    printf("///    O tema do nosso projeto é criar um sistema de agendamento         ///\n");
+    printf("///    para escritório de advocacia.                                     ///\n");
+    printf("///    Esse sistema foi desenvolvido na linguagem C, ele servirá         ///\n");
+    printf("///    para facilitar o agendamento de reuniões e eventos                ///\n");
+    printf("///    do setor jurídico, para que não hajam imprevistos com horários.   ///\n");
+    printf("///                                                                      ///\n");
+    printf("///            ========== O sistema conta com: ============              ///\n");
+    printf("///                                                                      ///\n");
+    printf("///                   ÁREA DO ADVOGADO;                                  ///\n");
+    printf("///                   ÁREA DO CLIENTE;                                   ///\n");
+    printf("///                   AGENDA GERAL;                                      ///\n");
+    printf("///                                                                      ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
     
     getchar();
     printf("Tecle ENTER para voltar para área pricipal do programa");
