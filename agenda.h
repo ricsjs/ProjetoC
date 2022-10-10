1cf31e2cbd70ddf31464ca28630e84c412848076
Ricardo <ricfilho0007@gmail.com>
2022-10-10 19:46:07 -0300

    Código separação da agenda geral - Ricardo e Janderson

---
+++ agenda.h
@@ -0,0 +1,5 @@
+void agenda_geral(void);
+void listar_eventos(void);
+void cadastrar_evento(void);
+void editar_evento(void);
+void remover_evento(void);
--- principal.c
+++ principal.c
@@ -3,16 +3,12 @@
 #include <locale.h>
 #include "advogado.h"
 #include "cliente.h"
+#include "agenda.h"
 
 //assinatura de telas
 void login(void);
 void principal(void);
 void sobre(void);
-void agenda_geral(void);
-void listar_eventos(void);
-void cadastrar_evento(void);
-void editar_evento(void);
-void remover_evento(void);
 
 int main(void){
     setlocale(LC_ALL,"portuguese");
@@ -114,181 +110,6 @@ void login(void){
 
     }
  }
-
-
-///////////////////////////////////////////////////////////////////////////
-////////////////////////////// A G E N D A ////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-
-void agenda_geral(void){
-    int opc;
-    while (opc != 0){
-    system("clear||cls");
-    printf("/////////////////////////////////////////////\n");
-    printf("///       ===== AGENDA GERAL =====        ///\n");
-    printf("/////////////////////////////////////////////\n");
-    printf("///                                       ///\n");
-    printf("///  1 - LISTAR EVENTOS                   ///\n");
-    printf("///  2 - CADASTRAR NOVO EVENTO            ///\n");
-    printf("///  3 - EDITAR EVENTO                    ///\n");
-    printf("///  4 - REMOVER EVENTO                   ///\n");
-    printf("///  0 - VOLTAR                           ///\n");
-    printf("/////////////////////////////////////////////\n");
-
-    printf("Digite o número de uma opção: ");
-    scanf("%i", &opc);
-
-    if (opc == 1){
-        listar_eventos();
-    }else if (opc == 2){
-        cadastrar_evento();
-    }else if(opc == 3){
-        editar_evento();
-    }else if(opc == 4){
-        remover_evento();
-    }else if (opc > 4 || opc < 0){
-      getchar();
-
-      printf("Número digitado não reconhecido, pressione enter para tentar novamente.");
-
-      printf("Número digitado não reconhecido, pressione enter para tentar novamente.");
-
-      getchar();
-    }
-    
-}
-}
-
-
-void listar_eventos(){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = = = = =         SIG-LAW         = = = = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///          Developed by @ricsjs & @janderson1111 -- since Ago, 2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                           LISTAGEM DE EVENTOS                           ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    getchar();
-    printf("Função ainda em desenvolvimento, tecle ENTER para voltar para a agenda geral");
-    getchar();
-}
-
-void cadastrar_evento(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = = = = =         SIG-LAW         = = = = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///          Developed by @ricsjs & @janderson1111 -- since Ago, 2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                            CADASTRAR EVENTO                             ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    
-    int token_evento, token_advogado, token_cliente;
-    char nome, data;
-
-    printf("Token do evento (apenas números): \n");
-    scanf("%i", &token_evento);
-    getchar();
-    printf("Nome do evento: \n");
-    scanf("%s", &nome);
-    getchar();
-    printf("Token do advogado vinculado ao evento (apenas números): \n");
-    scanf("%i", &token_advogado);
-    getchar();
-    printf("Token do cliente vinculado ao evento (apenas números): \n");
-    scanf("%i", &token_cliente);
-    getchar();
-    printf("Data do evento (dd/mm/aaaa): \n");
-    scanf("%s", &data);
-    getchar();
-    printf("Evento cadastrado com sucesso! Tecle ENTER para voltar para área a agenda geral");
-    getchar();
-    
-
-}
-
-void editar_evento(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = = = = =         SIG-LAW         = = = = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///          Developed by @ricsjs & @janderson1111 -- since Ago, 2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                              EDITAR EVENTO                              ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    
-    int token;
-    printf("Informe o token do evento para editar (apenas números): \n");
-    scanf("%i", &token);
-    getchar();
-    printf("Função ainda em desenvolvimento, tecle ENTER para voltar para a agenda geral");
-    getchar();
-    
-
-}
-void remover_evento(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = = = = =         SIG-LAW         = = = = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///          Developed by @ricsjs & @janderson1111 -- since Ago, 2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                             REMOVER EVENTO                              ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-
-    int token;
-    printf("Informe o token do evento para remover (apenas números): \n");
-    scanf("%i", &token);
-    getchar();
-    printf("Função ainda em desenvolvimento, tecle ENTER para voltar para a agenda geral");
-    getchar();
-    
-}
 
 
 void sobre(void){
