f155199b3aa51edd6d6b5a1725c2666998e93b2a
Ricardo <ricfilho0007@gmail.com>
2022-11-08 15:49:44 -0300

    correção de assinaturas

--- advogado.h
+++ advogado.h
@@ -1,7 +1,1 @@
-void menuPrincipaladv(void);
-void pesquisar_advogado(void);
-void cadastrar_advogado(void);
-void editar_advogado(void);
-void agenda_advogado(void);
-void remover_advogado(void);
 void menu_advogado(void);
--- cliente.h
+++ cliente.h
@@ -1,7 +1,1 @@
-void menuPrincipalcli(void);
-void pesquisar_cliente(void);
-void cadastrar_cliente(void);
-void editar_cliente(void);
-void agenda_cliente(void);
-void remover_cliente(void);
 void menu_cliente(void);
